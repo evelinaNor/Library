0d500782f54038b3fe04e0025521182e3364dc04
evelinaN <Evelina@DESKTOP-ROJME7Q>
2022-08-25 16:00:40 +0300

    update-annotation @Profile on security configuration to test controllers

--- Library/src/main/java/lt/codeacademy/learn/library/security/AppSecurity.java
+++ Library/src/main/java/lt/codeacademy/learn/library/security/AppSecurity.java
@@ -5,10 +5,12 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+//@Profile("test")
 @Configuration
 public class AppSecurity {
 	@Autowired
--- Library/src/main/java/lt/codeacademy/learn/library/security/SecurityConfiguration.java
+++ Library/src/main/java/lt/codeacademy/learn/library/security/SecurityConfiguration.java
@@ -1,12 +1,15 @@
 package lt.codeacademy.learn.library.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-
+//@Profile("test")
 @SuppressWarnings("deprecation")
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -24,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authenticated()
             .and()
             .formLogin().successHandler(successHandler);  
-        }          
- 
+        }
+	  
 	
 }
